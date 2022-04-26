f5959528094968785ab3a1fd70218373c16e9ad8
BadBoy0170 <44031565+BadBoy0170@users.noreply.github.com>
2022-04-27 00:46:44 +0530

    Add files via upload

---
+++ id and pass.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+char id[50];
+User:
+printf("Enter your id:\n");
+scanf("%s", &id);
+if (strcmp(id,"bd17")==0)
+{
+printf("Id is correct\n");
+}
+else
+{
+printf("You have enetred an invalid user id\nPlease enter id again\n");
+goto User;
+}
+char pass[50];
+pass:
+printf("Enter your password: ");
+scanf("%s", &pass);
+if(strcmp(pass,"bd157800")==0)
+{
+printf("You have successly loged in into your profile\n");
+}
+else
+{
+printf("You have entered a wrong password\nEnter your password again\n");
+goto pass;
+}
+return 0;
+}
